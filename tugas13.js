e2cf3f4c760f9a2b73ae696428f398720d265f9d
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 09:33:30 +0700

    TUGAS 13 JS

---
+++ tugas13.js
@@ -0,0 +1,7 @@
+    function tambahData() {
+        var namaBuah = ['pisang', 'jeruk']
+        console.log(namaBuah);
+        namaBuah.unshift('apel', 'mangga')
+        return namaBuah
+    }
+    console.log(tambahData());
